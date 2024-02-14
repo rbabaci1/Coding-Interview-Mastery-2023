3426fd6c995a16849c7ffba5efa77e1e604158b1
rabah babaci <4rabah@gmail.com>
2024-02-13 16:16:31 -0800

    solve problem #2

--- 1-Big-O/index.js
+++ 1-Big-O/index.js
@@ -1,18 +1,17 @@
 /********************* #1 ***********************/
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function funChallenge(input) {
-  let a = 10; // O(1)
-  a = 50 + 3; // O(1)
+	let a = 10; // O(1)
+	a = 50 + 3; // O(1)
 
-  for (let i = 0; i < input.length; i++) { // O(n)
-    anotherFunction(); // O(n)
-    let stranger = true; // O(n)
-    a++; // O(n)
-  }
-  return a; // O(1)
+	for (let i = 0; i < input.length; i++) {
+		// O(n)
+		anotherFunction(); // O(n)
+		let stranger = true; // O(n)
+		a++; // O(n)
+	}
+	return a; // O(1)
 }
-
-
 
 /*
     Solution: O(n).
@@ -27,21 +26,22 @@ function funChallenge(input) {
 /********************* #2 ***********************/
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function anotherFunChallenge(input) {
-    let a = 5; // O(1)
-    let b = 10; // O(1)
-    let c = 50; // O(1)
-    for (let i = 0; i < input; i++) { // O(n)
-      let x = i + 1; // O(n)
-      let y = i + 2; // O(n)
-      let z = i + 3; // O(n)
-
-    }
-    for (let j = 0; j < input; j++) { // O(n)
-      let p = j * 2; // O(n)
-      let q = j * 2; // O(n) 
-    }
-    let whoAmI = "I don't know"; // O(1)
-  }
+	let a = 5; // O(1)
+	let b = 10; // O(1)
+	let c = 50; // O(1)
+	for (let i = 0; i < input; i++) {
+		// O(n)
+		let x = i + 1; // O(n)
+		let y = i + 2; // O(n)
+		let z = i + 3; // O(n)
+	}
+	for (let j = 0; j < input; j++) {
+		// O(n)
+		let p = j * 2; // O(n)
+		let q = j * 2; // O(n)
+	}
+	let whoAmI = "I don't know"; // O(1)
+}
 
 /*
     Solution: O(n).
@@ -50,14 +50,18 @@ function anotherFunChallenge(input) {
 */
 
 /********************* #3 ***********************/
-const boxes = ['a', 'b', 'c', 'd', 'e'];
+const boxes = ["a", "b", "c", "d", "e"];
 
 function logAllPairsOfArray(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            console.log(arr[i] + '-' + arr[j])
-        }
-    }
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			console.log(arr[i] + "-" + arr[j]);
+		}
+	}
 }
 
-logAllPairsOfArray(boxes)
+/*
+    Solution: O(n^2). For n been the number of boxes.
+*/
+
+logAllPairsOfArray(boxes);
