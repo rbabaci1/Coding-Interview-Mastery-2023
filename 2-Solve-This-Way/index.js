2a46cbdf20e4a5dc94201fe3263febc377d29016
rabah babaci <4rabah@gmail.com>
2024-02-16 18:03:05 -0800

    define the hasPairWithSum to solve google interview question

---
+++ 2-Solve-This-Way/index.js
@@ -0,0 +1,7 @@
+// Using the interview cheat sheet taking the famoust google interview as an example
+
+function hasPairWithSum(nums, sum) {}
+
+const collection1 = [1, 2, 3, 9];
+const collection2 = [1, 2, 4, 4];
+const sum = 8;
