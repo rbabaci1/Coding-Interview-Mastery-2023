bdba41db831f4ebb40aa9afd2f9d957675b82101
rabah babaci <4rabah@gmail.com>
2024-02-20 18:47:28 -0800

    define the maxSubArray function

---
+++ 3-Data-Structures/1-Arrays/maxSubArray.js
@@ -0,0 +1,17 @@
+function maxSubArray(nums) {
+    
+}
+
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+const output = maxSubArray(nums);
+
+console.log(output);
+
+/* 
+    Example 1:
+
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+*/
