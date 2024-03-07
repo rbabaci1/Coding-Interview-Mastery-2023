10c8bd7bf6dd6035918dd9a4875843c94b5899cc
rabah babaci <4rabah@gmail.com>
2024-03-07 15:02:11 -0800

    implement a printTree method to display the tree with a clear visual

--- 3-Data-Structures/5-Trees/index.js
+++ 3-Data-Structures/5-Trees/index.js
@@ -11,6 +11,37 @@ class BinarySearchTree {
 		this.root = null;
 	}
 
+	printTree() {
+		if (!this.root) {
+			console.log("The tree is empty");
+			return;
+		}
+		let buffer = [];
+		this.traversePrint(this.root, "", true, (val) => buffer.push(val));
+		console.log("Root");
+		console.log(buffer.join(""));
+	}
+
+	traversePrint(node, prefix, isLeft, callback) {
+		if (node.right !== null) {
+			this.traversePrint(
+				node.right,
+				prefix + (isLeft ? "│   " : "    "),
+				false,
+				callback
+			);
+		}
+		callback(prefix + (isLeft ? "└── " : "┌── ") + node.value + "\n");
+		if (node.left !== null) {
+			this.traversePrint(
+				node.left,
+				prefix + (isLeft ? "    " : "│   "),
+				true,
+				callback
+			);
+		}
+	}
+
 	insert(value) {
 		const newNode = new Node(value);
 
@@ -18,7 +49,6 @@ class BinarySearchTree {
 			this.root = newNode;
 		} else {
 			let currentNode = this.root;
-
 			while (true) {
 				if (value < currentNode.value) {
 					if (!currentNode.left) {
@@ -45,10 +75,10 @@ const myTree = new BinarySearchTree();
 myTree.insert(9);
 myTree.insert(4);
 myTree.insert(6);
-// myTree.insert(20);
-// myTree.insert(170);
-// myTree.insert(15);
-// myTree.insert(1);
+myTree.insert(20);
+myTree.insert(170);
+myTree.insert(15);
+myTree.insert(1);
 
 /* 
         9
@@ -56,4 +86,4 @@ myTree.insert(6);
   1   6   15  170
 */
 
-console.log(myTree);
+myTree.printTree();
