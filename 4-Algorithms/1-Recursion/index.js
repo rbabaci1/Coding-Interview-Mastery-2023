b3de967dac799f7b7dcfe371355243473c2a9e9d
rabah babaci <4rabah@gmail.com>
2024-03-15 16:56:06 -0700

    implement fibonacci recursively and iteratively

--- 4-Algorithms/1-Recursion/index.js
+++ 4-Algorithms/1-Recursion/index.js
@@ -10,11 +10,35 @@ function findFactorialIterative(num) {
 }
 
 function findFactorialRecursive(num) {
-	if (num === 1) return num;
+	if (num === 2) return num;
 
 	return findFactorialRecursive(num - 1) * num;
 }
 
-console.log(findFactorialIterative(5)); // 120
+// console.log(findFactorialIterative(5)); // 120
 
-console.log("Factorial: " + findFactorialRecursive(5)); // 120
+// console.log("Factorial: " + findFactorialRecursive(5)); // 120
+
+function fibIterative(n) {
+	// n = 5 -->  0, 1, 1, 2, 3, 5, 8, 13, 21
+	let a = 0;
+	let b = 1;
+	let result = 0;
+	while (n >= 2) {
+		result = a + b;
+		a = b;
+		b = result;
+		n--;
+	}
+
+	return result;
+}
+
+function fibRecursive(n) {
+	if (n < 2) return n;
+
+	return fibRecursive(n - 1) + fibRecursive(n - 2);
+}
+
+// console.log(fibIterative(40));
+// console.log(fibRecursive(40));
