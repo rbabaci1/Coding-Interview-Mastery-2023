0dda1bc8bb4444c87abde3804117deb8f3ba77e4
rabah babaci <4rabah@gmail.com>
2024-03-18 20:55:35 -0700

    implement the bubble sort algo

--- 4-Algorithms/2-Sorting/index.js
+++ 4-Algorithms/2-Sorting/index.js
@@ -1,7 +1,15 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
-	//Code here
+	for (let i = 0; i < array.length; i++) {
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[i]) {
+				[array[i], array[j]] = [array[j], array[i]];
+			}
+		}
+	}
+
+	return array;
 }
 
 bubbleSort(numbers);
