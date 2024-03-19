5ff48f09a62e309c22c75359c22a91d07e116677
rabah babaci <4rabah@gmail.com>
2024-03-18 21:22:36 -0700

    refactor the bubble sort for better perfomance

--- 4-Algorithms/2-Sorting/index.js
+++ 4-Algorithms/2-Sorting/index.js
@@ -1,15 +1,18 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
-	for (let i = 0; i < array.length; i++) {
-		for (let j = i + 1; j < array.length; j++) {
-			if (array[j] < array[i]) {
-				[array[i], array[j]] = [array[j], array[i]];
+	let swappOccured;
+
+	do {
+		swappOccured = false;
+
+		for (let i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				[array[i], array[i + 1]] = [array[i + 1], array[i]]; // Swap elements
+				swappOccured = true;
 			}
 		}
-	}
-
-	return array;
+	} while (swappOccured);
 }
 
 bubbleSort(numbers);
